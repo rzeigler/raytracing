198bcd0dea64718796c6aaa1c365acf7bc55180c
Ryan Zeigler <zeiglerr@gmail.com>
2020-06-14 14:53:24 -0500

    Restart

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,53 @@
+#![warn(clippy::all)]
 use anyhow::{Context, Result};
-use clap::{App, Arg};
+use clap::{value_t, App, Arg};
 use std::fs::File;
 use std::path::Path;
 
-mod data;
-mod ppm;
-mod tracer;
+const IMAGE_WIDTH: usize = 1600;
+const IMAGE_HEIGHT: usize = 800;
 
-use data::*;
-
-const OUT_PARAM: &str = "out";
-
-fn main() {
+fn main() -> Result<()> {
     let matches = App::new("raytracing")
         .version("chapter-1")
         .author("Ryan Zeigler <zeiglerr@gmail.com>")
         .about("Implementation of the raytracing from https://raytracing.github.io/ to learn Rust")
         .arg(
-            Arg::with_name(OUT_PARAM)
-                .short("o")
-                .long("out")
+            Arg::with_name("width")
+                .short("w")
+                .long("width")
+                .takes_value(true)
+                .help("The width in pixels of the generated image"),
+        )
+        .arg(
+            Arg::with_name("height")
+                .short("h")
+                .long("height")
+                .takes_value(true)
+                .help("The height in pixels of the generated image"),
+        )
+        .arg(
+            Arg::with_name("out")
                 .value_name("FILE")
                 .takes_value(true)
+                .required(true)
                 .help("The path to write output too"),
         )
         .get_matches();
-    let out_path = Path::new(matches.value_of(OUT_PARAM).unwrap());
-
-    if let Err(e) = run(&out_path) {
-        eprintln!("raytracing failed:\n{}", e);
-        std::process::exit(1);
-    }
+    let width = if matches.is_present("width") {
+        value_t!(matches, "width", usize).with_context(|| "invalid width")?
+    } else {
+        IMAGE_WIDTH
+    };
+    let height = if matches.is_present("height") {
+        value_t!(matches, "height", usize).with_context(|| "invalid height")?
+    } else {
+        IMAGE_WIDTH
+    };
+    let out_path = Path::new(matches.value_of("out").unwrap());
+    run(width, height, &out_path)
 }
 
-const IMAGE_WIDTH: usize = 1600;
-const IMAGE_HEIGHT: usize = 800;
-
-fn run(out_path: &Path) -> Result<()> {
-    let canvas = tracer::draw(IMAGE_HEIGHT, IMAGE_WIDTH);
-    let mut out_file = File::create(out_path)
-        .with_context(|| format!("unable to open {} for writting", out_path.to_string_lossy()))?;
-
-    ppm::write(&canvas, &mut out_file)?;
-
+fn run(width: usize, height: usize, out_path: &Path) -> Result<()> {
     Ok(())
 }
